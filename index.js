ce337336ccd4b4801749d04410a125393b74caf6
ataur <amirataur@gmail.com>
2021-11-04 23:38:13 +0600

    first commt

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const uri = "mongodb+srv://visitbd:zXFcAaswUPmjZmjM@cluster0.svurl.mongodb.net/m
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const serviceColection = client.db("VistBD").collection("Services");
+    const ordersColection = client.db("VistBD").collection("orders");
 
     app.get("/", (req, res) => {
         console.log("ami home teke")
@@ -35,21 +36,23 @@ client.connect(err => {
         res.send(result)
         // console.log(result)
       })
+
+  
   
         //get one from service
 
-    //  app.get('/services/:id',async(req,res)=>{
-    //     const id = req.params.id
-    //     console.log(id)
-    //     if(id){
-    //     const query = { _id: ObjectId(id) }
-    //     const result = await serviceColection.findOne(query)
-    //     res.send(result)
-    //     // console.log(result)
+     app.get('/services/:id',async(req,res)=>{
+        const id = req.params.id
+        console.log(id)
+        if(id){
+        const query = { _id: ObjectId(id) }
+        const result = await serviceColection.findOne(query)
+        res.send(result)
+        // console.log(result)
   
-    //     }
+        }
         
-    //   })
+      })
 
       //add service  by post method
     app.post('/services',async(req,res)=>{
@@ -59,9 +62,55 @@ client.connect(err => {
   
         const result = await serviceColection.insertOne(newService)
         res.send(result)
-        console.log(result)
+        // console.log(result)
   
       })
+
+app.post('/orders',async(req,res)=>{
+  console.log("from orders post");
+  const newOrder = req.body;
+  const result = await ordersColection.insertOne(newOrder)
+  res.send(result)
+  console.log(result);
+})
+  
+
+app.get('/orders',async(req,res)=>{
+  const result = await ordersColection.find({}).toArray()
+  res.send(result)
+})
+
+app.get('/orders/:email',async(req,res)=>{
+  const email = req.params.email;
+  const result = await ordersColection.find({emial:email}).toArray()
+  res.send(result)
+  console.log(result);
+})
+
+app.put('/orders/:id',async(req,res)=>{
+  const id = req.params.id
+  const filter = { _id: ObjectId(id) };
+  
+  const options = { upsert: true };
+
+  const updateDoc = {
+    $set: {
+      approved:true
+    },
+  };
+  const result = await ordersColection.updateOne(filter, updateDoc, options);
+
+
+})
+
+app.delete('/orders/:id',async(req,res)=>{
+  const id = req.params.id
+  const deleteID = {_id:ObjectId(id)}
+  const result = await ordersColection.deleteOne(deleteID)
+  res.send(result)
+  console.log(result);
+})
+
   
   
     // perform actions on the collection object
