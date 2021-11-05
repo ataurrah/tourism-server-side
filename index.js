5b761b778a8b9947ac075803f9f65c036af97d7c
Mohammad Ataur Rahman Sikder <86624766+ataurrah@users.noreply.github.com>
2021-11-05 16:37:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://visitbd:zXFcAaswUPmjZmjM@cluster0.svurl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.svurl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const serviceColection = client.db("VistBD").collection("Services");
